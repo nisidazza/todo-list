d3aa58504e72df78296470b0e9494df9557d9f12
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-29 16:54:49 +1200

    add onChange event handler that calls toggleTodo

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -7,11 +7,15 @@ interface TodoListItemProps {
   toggleTodo: ToggleTodo;
 }
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodo }) => {
   return (
     <li>
       <label className={todo.complete ? "complete" : undefined}>
-        <input type="checkbox" checked={todo.complete} />
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />
         {todo.text}
       </label>
     </li>
