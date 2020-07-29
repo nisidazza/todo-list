b4a9f8f0a64e4ace825d7784e98d8ade5ba8515b
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-29 16:17:08 +1200

    create an array of todos'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import { TodoListItem } from "./TodoListItem";
 
+//conformed to the todo in TodoListItemProps
+type Todo = {
+  text: string;
+  complete: boolean;
+}
+
+
+const todos = [{text:"Walk the dog", complete: true},
+{text: "Write app", complete: false}
+]
+
 const App: React.FC = () => {
   return (
     <div>
