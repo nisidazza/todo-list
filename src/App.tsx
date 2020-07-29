063350d9a2eacf55843e43493574ec7f10c171f8
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-29 16:38:24 +1200

    add a state for my todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItem } from "./TodoListItem";
 import { Todo } from "./types";
 
 //type of Array that takes a generic Todo
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "Walk the dog", complete: true },
   { text: "Write app", complete: false },
 ];
 
 const App: React.FC = () => {
+  //add state
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
     <div>
       <TodoListItem todo={todos[0]} />
