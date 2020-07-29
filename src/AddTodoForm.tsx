51cf280a22625f5e9951aa3d9c671f41e06843d7
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-07-30 10:27:40 +1200

    add change handler to input tag

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, ChangeEvent } from "react";
 
 const AddTodoForm: React.FC = () => {
-
   const [newTodo, setNewTodo] = useState("");
-  
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value);
+  };
+
   return (
     <form>
-      <input type="text" value={newTodo}/>
+      <input type="text" value={newTodo} onChange={handleChange} />
       <button type="submit">Add Todo</button>
     </form>
   );
